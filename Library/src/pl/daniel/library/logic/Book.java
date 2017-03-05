58beab73422f0512ffe9dcf37ee2971b2078bfb7
Daniel <Daniel@Laptop>
2017-03-05 19:37:47 +0100

    first commit

---
+++ Library/src/pl/daniel/library/logic/Book.java
@@ -0,0 +1,15 @@
+package pl.daniel.library.logic;
+/**
+ * Class Book contains information about books 
+ * in library
+ * @author Daniel
+ *
+ */
+public class Book {
+	String title;
+	String author;
+	int releaseDate;
+	int pages;
+	String publisher;
+	String isbn;
+}
---
+++ Library/src/pl/daniel/library/logic/Library.java
@@ -0,0 +1,31 @@
+package pl.daniel.library.logic;
+
+/**
+ *  @author Daniel 
+ * Main class o library applictation
+ */
+public class Library {
+	public static void main(String[] args) {
+
+		final String appName = "Biblioteka v0.2";
+		
+		Book book1 = new Book();
+		book1.title = "W pustyni i w puszczy";
+		book1.author = "Henryk Sienkiewicz";
+		book1.releaseDate = 2010;
+		book1.pages = 296;
+		book1.publisher = "Greg";
+		book1.isbn = "978313333211";
+
+		System.out.println(appName);
+		System.out.println("Ksi¹zki dostêpne w bibliotece");
+		System.out.println(book1.title);
+		System.out.println(book1.author);
+		System.out.println(book1.releaseDate);
+		System.out.println(book1.pages);
+		System.out.println(book1.publisher);
+		System.out.println(book1.isbn);
+
+	}
+
+}
