cc2d7fffab1d590d68c075a9aa7d2900077cfb17
Daniel <Daniel@Laptop>
2017-03-06 14:56:07 +0100

    cbv

--- Library/src/pl/daniel/library/app/Library.java
+++ Library/src/pl/daniel/library/app/Library.java
@@ -1,30 +1,29 @@
-package pl.daniel.library.logic;
+package pl.daniel.library.app;
+
+import pl.daniel.library.data.Book;
+import pl.daniel.library.utils.DataReader;
 
 /**
- *  @author Daniel 
- * Main class o library applictation
+ * @author Daniel Main class o library applictation
  */
 public class Library {
 	public static void main(String[] args) {
 
-		final String appName = "Biblioteka v0.2";
-		
-		Book book1 = new Book();
-		book1.title = "W pustyni i w puszczy";
-		book1.author = "Henryk Sienkiewicz";
-		book1.releaseDate = 2010;
-		book1.pages = 296;
-		book1.publisher = "Greg";
-		book1.isbn = "978313333211";
+		final String appName = "Biblioteka v0.6";
+
+		Book[] books = new Book[1000];
+		DataReader dataReader = new DataReader();
 
 		System.out.println(appName);
-		System.out.println("Ksi¹zki dostêpne w bibliotece");
-		System.out.println(book1.title);
-		System.out.println(book1.author);
-		System.out.println(book1.releaseDate);
-		System.out.println(book1.pages);
-		System.out.println(book1.publisher);
-		System.out.println(book1.isbn);
+		System.out.println("Wprowadz now¹ ksi¹¿kê: ");
+		books[0] = dataReader.readAndCreateBook();
+		books[1] = dataReader.readAndCreateBook();
+		dataReader.close();
+
+		books[0].printInfo();
+		books[1].printInfo();
+
+		System.out.println("System mo¿e przechowywaæ do " + books.length + " ksi¹¿ek.");
 
 	}
 
